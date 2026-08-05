d8e9b0e92112788f00fee6d6a3d7f4398e609a4d
saimohanrajareddyjannareddy <aiwithsai17@gmail.com>
2026-08-04 22:03:07 -0500

    Time: 13 ms (51.88%), Space: 19.4 MB (46.15%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        counts={}
+        for ch in s:
+            counts[ch]=counts.get(ch,0)+1
+        for ch in t:
+            counts[ch]=counts.get(ch,0)-1
+        for count in counts.values():
+            if count!=0:
+                return False
+        return True
